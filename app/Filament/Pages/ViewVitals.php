a7fa4575051168942bce205395ba4c17f6b713e8
Your Name <your.email@example.com>
2025-10-28 06:35:19 +0000

    Disable ViewVitals page navigation to prevent Resident Care dropdown
    - Added shouldRegisterNavigation = false to ViewVitals page
    - This prevents the Resident Care dropdown from appearing
    - Ensures only custom navigation is used

--- app/Filament/Pages/ViewVitals.php
+++ app/Filament/Pages/ViewVitals.php
@@ -8,6 +8,7 @@ class ViewVitals extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static ?string $navigationGroup = 'Resident Care';
+    protected static bool $shouldRegisterNavigation = false;
 
     protected static string $view = 'filament.pages.view-vitals';
 }
